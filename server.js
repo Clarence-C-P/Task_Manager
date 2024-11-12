13330f74f50d57f432490df396c978bd564f1344
Clarence-C-P <clarencedc129@gmail.com>
2024-11-12 13:41:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -46,9 +46,14 @@ async function connectToDatabase() {
 connectToDatabase();
 
 // MongoDB connection using the URI from .env
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((error) => console.error('MongoDB connection error:', error));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
 
 // User Schema
 const userSchema = new mongoose.Schema({
